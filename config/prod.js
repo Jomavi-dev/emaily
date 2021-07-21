2aed471987f2cb237690bd7b84813d8af699cbf9
Mavi <jomavix@hotmail.com>
2021-07-21 03:37:19 +0100

    correct wrong googleClientSecret key

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@
 //get production keys from environment variables
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googlemongoURI: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
